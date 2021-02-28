a8e2d23dea5ec5e90a03747dd72c786593c623ca
Tsapu <davis.kruumins@gmail.com>
2021-02-28 16:17:04 +0200

    idea page constructed

--- js/main.js
+++ js/main.js
@@ -1,82 +1,108 @@
 
 jQuery.fn.visible = function() {
-    return this.css('visibility', 'visible');
+    return this.css("visibility", "visible");
 };
 
 jQuery.fn.invisible = function() {
-    return this.css('visibility', 'hidden');
+    return this.css("visibility", "hidden");
 };
 
 
 // Menu:
-$('.menu-icon').click(function() {
+$(".menu-icon").click(function() {
 	
-	$('.menu-icon').toggle();
-	$('.menu-close').toggle();
+	$(".menu-icon").toggle();
+	$(".menu-close").toggle();
 	
-	// No need to adjust for viewport since the icon's clickability already accounts for it
+	// No need to adjust for viewport since the icon"s clickability already accounts for it
 	
-	$('#nav-list').toggleClass('nav-list'); // Remove desktop styling
-	$('#menu').addClass('menu-list'); // Add m&t styling
-	$('#menu').prepend($ ('#menu>li:last-child') ) // Change list element position last->first
-	$('#nav-list').visible(); // Show menu
+	$("#nav-list").toggleClass("nav-list"); // Remove desktop styling
+	$("#menu").addClass("menu-list"); // Add m&t styling
+	$("#menu").prepend($ ("#menu>li:last-child") ) // Change list element position last->first
+	$("#nav-list").visible(); // Show menu
 })
 
-$('.menu-close').click(function() {
-	$('.menu-icon').toggle();
-	$('.menu-close').toggle();
+$(".menu-close").click(function() {
+	$(".menu-icon").toggle();
+	$(".menu-close").toggle();
 	
-	$('#nav-list').toggleClass('nav-list');
-	$('#menu').removeClass('menu-list');
+	$("#nav-list").toggleClass("nav-list");
+	$("#menu").removeClass("menu-list");
 	
-	$('#menu').append($ ('#menu>li:first-child') );
-	$('#nav-list').invisible();
+	$("#menu").append($ ("#menu>li:first-child") );
+	$("#nav-list").invisible();
 })
 
 
 // Video controls:
-$('.play-icon').click(function() {
-	$('.main-vid')[0].load();
-	$('.main-vid')[0].play();
-	$('.main-vid').toggle();
-	$('.play-icon').toggle();
-	$('.close-icon').toggle();
+$(".play-icon").click(function() {
+	$(".main-vid")[0].load();
+	$(".main-vid")[0].play();
+	$(".main-vid").toggle();
+	$(".play-icon").toggle();
+	$(".close-icon").toggle();
 })
 
-$('.close-icon').click(function() {
-	$('.main-vid')[0].pause();
-	$('.main-vid').toggle();
-	$('.play-icon').toggle();
-	$('.close-icon').toggle();
+$(".close-icon").click(function() {
+	$(".main-vid")[0].pause();
+	$(".main-vid").toggle();
+	$(".play-icon").toggle();
+	$(".close-icon").toggle();
 })
 
 
 // Form control:
-$('#data-entry-consent').click(function(){
+$("#data-entry-consent").click(function(){
 	
-	$('#consent-prompt').css({'outline': 'none'})
-	$('.info-message, .failure').hide();
+	$("#consent-prompt").css({"outline": "none"})
+	$(".info-message, .failure").hide();
 	
 })
 
+// Carousel:
+
+let direction;
+$('#quote-slider').on('slid.bs.carousel', function () {
+  if ( $("#quote-first").hasClass("active") ) {
+		direction = "prev";	
+		} else if ( $("#quote-last").hasClass("active") ) {
+			direction = "next";
+		} else {
+			direction = "";
+			$(".quote-nav-prev, .quote-nav-next").css({"opacity": 1})
+		}
+	$(".quote-nav-" + direction).css({"opacity": .5})
+})
+
+/* $(".quote-selection")[0].addEventListener("click", () => {
+	console.log("clicked");
+	let direction;
+	if ( $("#quote-first").hasClass("active") ) {
+		direction = "prev";	
+		} else if ( $("#quote-last").hasClass("active") ) {
+			direction = "next";
+		} else {direction = "";}
+	$(".quote-nav-" + direction).css({"opacity": .5})
+}); */
+
 
 let formData;
-$('#btn-submit').click(function() {
+$("#btn-submit").click(function() {
 	
-	let consent = $('#data-entry-consent').prop('checked');
+	let consent = $("#data-entry-consent").prop("checked");
 	
 	
-	if (!$('#name-entry')[0].checkValidity() || !$('#email-entry')[0].checkValidity() || !$('#phone-entry')[0].checkValidity()){
+	if (!$("#name-entry")[0].checkValidity() || !$("#email-entry")[0].checkValidity() || !$("#phone-entry")[0].checkValidity()){
 		
 		return;
 		
 	} else	if (consent == false) {
-		$('#consent-prompt').css({'outline': '2px solid rgb(235,35,52)', 'outline-offset': '3px'});
-		$('.failure-msg').show();
+		$("#consent-prompt").css({"outline": "2px solid rgb(235,35,52)", "outline-offset": "3px"});
+		$(".failure-msg").show();
 		
 	} else {
 		
-		let $formRaw = $('.form-control');
+		let $formRaw = $(".form-control");
 		
 		formData = {
 			name: $formRaw[0].value,
@@ -85,30 +111,28 @@ $('#btn-submit').click(function() {
 		}
 		
 		if ($formRaw.length <= 4) {
-			formData['textMessage'] = $formRaw[3].value;
+			formData["textMessage"] = $formRaw[3].value;
 		}
 
 		console.log(formData);	
-		console.log($('.form-control')[0].value.split(' ')[0]);
+		console.log($(".form-control")[0].value.split(" ")[0]);
 		
-		$('.success-msg').show().fadeOut(3000);
+		$(".success-msg").show().fadeOut(3000);
 		
 		for (let i = 0; i < $formRaw.length; i++) {
-			$formRaw[i].value = '';
+			$formRaw[i].value = "";
 		}
 	}
 })
 
-$('form').submit(function(event) {
+$("form").submit(function(event) {
 	event.preventDefault();
 })
 
 // To top
- $('.footer-logo').click(function() {
-	window.scroll({top: 0, behavior: 'smooth'});	
+ $(".footer-logo").click(function() {
+	window.scroll({top: 0, behavior: "smooth"});	
 })
-
-
 
 
 
